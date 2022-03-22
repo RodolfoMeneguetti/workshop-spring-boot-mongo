dada7b0fb5028942ffc62fbd1c1f0a387cfcdd4f
RodolfoMeneguetti <paulo.meneguettitec@gmail.com>
2022-03-22 16:37:44 -0300

    Inserção de usuario com POST

--- src/main/java/com/goodyear/workshopmongo/Service/UserService.java
+++ src/main/java/com/goodyear/workshopmongo/Service/UserService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import com.goodyear.workshopmongo.ServiceException.ObjectNotFoundException;
 import com.goodyear.workshopmongo.domain.User;
+import com.goodyear.workshopmongo.dto.UserDTO;
 import com.goodyear.workshopmongo.repository.UserRepository;
 
 @Service
@@ -23,5 +24,13 @@ public class UserService {
 	public User findById(String id) {
 		Optional<User> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+	
+	public User insert(User obj) {
+		return repo.insert(obj);
+	}
+	
+	public User fromDTO(UserDTO objDTO) {
+		return new User (objDTO.getId(),objDTO.getName(),objDTO.getEmail());
 	}
 }
